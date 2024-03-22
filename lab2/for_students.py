a38bbcadb2d742b8120778c26095403da5e6a528
Adam Pacek <61830085+AdasPa@users.noreply.github.com>
2024-03-22 01:50:41 +0100

    Updated algorithm

--- lab2/for_students.py
+++ lab2/for_students.py
@@ -8,6 +8,9 @@ from data import *
 def initial_population(individual_size, population_size):
     return [[random.choice([True, False]) for _ in range(individual_size)] for _ in range(population_size)]
 
+def create_random(individual_size):
+    return [random.choice([True, False]) for _ in range(individual_size)]
+
 def fitness(items, knapsack_max_capacity, individual):
     total_weight = sum(compress(items['Weight'], individual))
     if total_weight > knapsack_max_capacity:
@@ -25,53 +28,38 @@ def population_best(items, knapsack_max_capacity, population):
     return best_individual, best_individual_fitness
 
 
-items, knapsack_max_capacity = get_big()
-print(items)
-
-population_size = 100
-generations = 200
-n_selection = 20
-n_elite = 1
-
-candidates_number = 30
-kindermachen_sessions = 2
-
-start_time = time.time()
-best_solution = None
-best_fitness = 0
-population_history = []
-best_history = []
-population = initial_population(len(items), population_size)
-population.sort(key=lambda ind: fitness(items, knapsack_max_capacity, ind), reverse=True)
-
 def choose_candidates(items, knapsack_max_capacity, population, candidates_number):
     candidates = population[:candidates_number]
-    #candidates = random.sample(population, candidates_number)
+    # candidates = random.sample(population, candidates_number)
     candidates_fitness = [fitness(items, knapsack_max_capacity, ind) for ind in candidates]
     return candidates, candidates_fitness
+
 
 def select_parent(candidates, candidates_fitness):
     total_fitness = sum(candidates_fitness)
     probabilities = [fitness_value / total_fitness for fitness_value in candidates_fitness]
     selected_index = random.choices([i for i in range(len(candidates))], weights=probabilities)[0]
     return candidates[selected_index]
+
+
 def selection(items, knapsack_max_capacity, population, n_selection):
     selected_parents = []
     candidates, candidates_fitness = choose_candidates(items, knapsack_max_capacity, population, candidates_number)
-    
+
     for _ in range(n_selection):
         parent1 = select_parent(candidates, candidates_fitness)
         parent2 = select_parent(candidates, candidates_fitness)
         selected_parents.append((parent1, parent2))
     return selected_parents
 
+
 def crossover(parents, kindermachen_sessions):
     children = []
     for parent1, parent2 in parents:
-        #crossover_point = random.randint(1, len(parent1) - 1)
-        #child1 = parent1[:crossover_point] + parent2[crossover_point:]
-        #child2 = parent2[:crossover_point] + parent1[crossover_point:]
-        #children.extend([child1, child2])
+        # crossover_point = random.randint(1, len(parent1) - 1)
+        # child1 = parent1[:crossover_point] + parent2[crossover_point:]
+        # child2 = parent2[:crossover_point] + parent1[crossover_point:]
+        # children.extend([child1, child2])
 
         for _ in range(kindermachen_sessions):
             child = []
@@ -84,6 +72,7 @@ def crossover(parents, kindermachen_sessions):
             children.append(child)
     return children
 
+
 def mutation(children):
     for child in children:
         for i in range(1):
@@ -91,11 +80,29 @@ def mutation(children):
             child[mutation_point] = not child[mutation_point]
 
 
+items, knapsack_max_capacity = get_big()
+print(items)
+
+population_size = 100
+generations = 200
+n_selection = 20
+n_elite = 99
+
+candidates_number = 30
+kindermachen_sessions = 2
+
+start_time = time.time()
+best_solution = None
+best_fitness = 0
+population_history = []
+best_history = []
+population = initial_population(len(items), population_size)
+population.sort(key=lambda ind: fitness(items, knapsack_max_capacity, ind), reverse=True)
 
 for _ in range(generations):
     population_history.append(population)
 
-    # TODO: implement genetic algorithm
+    # DONE: implement genetic algorithm
     selected_parents = selection(items, knapsack_max_capacity, population, n_selection)
     children = crossover(selected_parents, kindermachen_sessions)
     mutation(children)
@@ -103,8 +110,9 @@ for _ in range(generations):
     combined_population = population + children
     combined_population.sort(key=lambda ind: fitness(items, knapsack_max_capacity, ind), reverse=True)
     population = combined_population[:population_size]
-
-
+    #add randoms
+    randoms = [create_random(len(population[0])) for _ in range(population_size-n_elite)]
+    population = population[:-(population_size-n_elite)] + randoms
 
     best_individual, best_individual_fitness = population_best(items, knapsack_max_capacity, population)
     if best_individual_fitness > best_fitness:
@@ -121,7 +129,7 @@ print('Time: ', total_time)
 # plot generations
 x = []
 y = []
-top_best = 100
+top_best = 10
 for i, population in enumerate(population_history):
     plotted_individuals = min(len(population), top_best)
     x.extend([i] * plotted_individuals)
